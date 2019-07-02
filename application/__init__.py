6314e272ab0fc62837c57bd8f3f7700cf03036c8
Konstantin <diarts@mail.ru>
2019-07-02 16:50:42 +0300

    Add application generate function
    Add application generate function, at fabric pattern

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,25 @@
 from flask import Flask
-from config import Config
+from config import config
+from database import db
+from application.main import main
+from application.users import users
+from application.blogs import blogs
 
 
-def create_app(config=Config):
+def create_app(config_name=config.get('default')):
+    if not config_name:
+        raise ValueError('default configuration for flask application does not exist.')
+    check_config = config.get(config_name)
+    if not check_config:
+        raise ValueError(f'config whith name {config_name} does not exist.')
+
     app = Flask(__name__)
+    app.config.from_object(check_config)
+
+    db.init_app(app)
+
+    app.register_blueprint(main)
+    app.register_blueprint(users, url_prefix='/users')
+    app.register_blueprint(blogs, url_prefix='/blogs')
 
     return app
